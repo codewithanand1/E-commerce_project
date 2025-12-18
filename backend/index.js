afaa237a8304ee855394ebed044b327f42c4173e
Anand Verma <anandsen4822@gmail.com>
2025-12-18 16:50:31 +0530

    uuuu

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
     {
-    origin:["http://localhost:5173","http://localhost:5174","https://e-commerce-fronted-r89c.onrender.com"],
+    origin:"*",
     credentials:true
     }
 ))
