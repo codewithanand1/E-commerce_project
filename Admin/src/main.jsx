79048c3cefc4d8fd07533c6f346e3888b0590099
Anand Verma <anandsen4822@gmail.com>
2025-12-18 16:14:29 +0530

    url

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AdminContext from './context/AdminContext.jsx'
-export const serverurl="http://localhost:8000"
+export const serverurl="https://e-commerce-backend-n1dw.onrender.com"
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AdminContext>
